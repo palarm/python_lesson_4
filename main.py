be829ed10890c2d761605677aadcdfba67e1dc38
palarm <newton@ngs.ru>
2020-06-17 13:09:59 +0700

    v 1.0

---
+++ main.py
@@ -0,0 +1,79 @@
+from random import choice
+import string
+import itertools
+import operator
+from collections import Counter
+import re
+
+# воспользовался текстовым файлом с именами из другой задачи
+with open('randomNames.txt', encoding='utf-8') as fl: namesList = [line.strip() for line in fl]
+'''
+    1. Напишите функцию (F): на вход список имен и целое число N; на выходе список длины N случайных имен из первого списка
+    (могут повторяться, можно взять значения: количество имен 20, N = 100, рекомендуется использовать функцию random);
+'''
+def f(namesList, N):
+    '''
+    :param1: список имен
+    :param2: длина списка имен
+    :return: список имен запрашиваемой длины (N)
+
+    '''
+    return [choice(namesList) for _ in range(N)]
+
+bigNamesList = f(namesList, 100)
+print(bigNamesList)
+
+'''
+    2. Напишите функцию вывода самого частого имени из списка на выходе функции F;
+'''
+def getPopName(bigNamesList):
+
+# # Вариант 1 (работает только для первого вхождения)
+#     return max(set(bigNamesList), key=bigNamesList.count)
+
+# Вариант 2 (возвращает словарь вида {'имя':'частота встречаемости'})
+    bigNamesCounter = Counter(bigNamesList)
+    maxC = max(bigNamesCounter.values())
+    return {k: v for k, v in bigNamesCounter.items() if v == maxC}
+
+moustPopNames = getPopName(bigNamesList)
+print(moustPopNames)
+
+'''
+    3. Напишите функцию вывода самой редкой буквы, с которого начинаются имена в списке на выходе функции F.
+'''
+
+def getInfreqLetter(bigNamesList):
+
+
+# # Вариант 1 (только первое найденное минимальное значение)
+#     return min(set(x[0] for x in bigNamesList), key=bigNamesList.count)
+
+# Вариант 2 (возвращает словарь вида {'Первая буква имени':'частота встречаемости'})
+
+    InfreqFirstLetter = Counter([x[0] for x in bigNamesList])
+    minL = min(InfreqFirstLetter.values())
+    return {k: v for k, v in InfreqFirstLetter.items() if v == minL}
+
+
+firstInfreqLetter = getInfreqLetter(bigNamesList)
+print(firstInfreqLetter)
+
+
+
+'''
+4.  В файле с логами найти дату самого позднего лога (по метке времени)
+'''
+
+pattern = re.compile('\d\d:\d\d:\d\d')
+maxTime = ''
+dateLine = 0
+# print(pattern.findall(line))
+with open('log') as fl:
+    for idx, line in enumerate(fl):
+        if maxTime < max(pattern.findall(line)):
+            maxTime = max(pattern.findall(line))
+            dateLine = line
+            # print(dateLine)
+    # fl.readline(dateLine)
+print(dateLine[:10])
